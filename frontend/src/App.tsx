c68fee4a02433d0ec6dfe8508ef7041ab6894bbd
Roshan Reji <roshanreji987@gmail.com>
2025-11-16 15:39:47 +0530

    initial frontend scaffoldings:(reactvite,tyepscript,eslint,prettier,tailwind,other npm needs for articly)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="space-y-4 p-6">
+      <div className="text-red-500">Red Text</div>
+      <div className="text-green-500">Green Text</div>
+      <div className="text-blue-500">Blue Text</div>
+    </div>
+  )
+}
+
+export default App
